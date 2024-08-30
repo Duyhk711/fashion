a55e9e6d51702ef4b5aa1c21eb01ddfd871a0cf4
your_username <nguyentaiviet9@gmai.com>
2024-08-30 14:31:23 +0700

    initial commit

--- routes/client.php
+++ routes/client.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/home', 'client.home');
 Route::view('/shop', 'client.shop');
 Route::view('/empty', 'client.empty');
+Route::view('/myaccount', 'client.my-account');
