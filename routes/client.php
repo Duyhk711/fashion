70ca7e2098f527a53fb94a86c96df51bbb8d7d1e
your_username <nguyentaiviet9@gmai.com>
2024-08-30 13:35:31 +0700

    initial commit

--- routes/client.php
+++ routes/client.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,16 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
- */
+*/
 
-Route::view('/', 'landing');
-Route::match(['get', 'post'], '/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-Route::view('/pages/slick', 'pages.slick');
-Route::view('/pages/datatables', 'pages.datatables');
-Route::view('/pages/blank', 'pages.blank');
-
-Route::prefix('admin')
-    ->as('admin.')
-    ->group(function () {});
+// Example Routes
+Route::view('/home', 'client.home');
