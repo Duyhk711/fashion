d49f1e54a1cec5667e3a4e332ca68d6eeb466d0a
your_username <nguyentaiviet9@gmai.com>
2024-09-05 16:24:41 +0700

    initial commit

--- routes/client.php
+++ routes/client.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
  */
 
 // Example Routes
-Route::view('/home', 'client.home');
-Route::view('/shop', 'client.shop');
-Route::view('/empty', 'client.empty');
-Route::view('/checkout', 'client.checkout');
-Route::view('/order-success', 'client.order-success');
-Route::view('/wishlist', 'client.wishlist');
-Route::view('/empty', 'client.empty');
-Route::view('/cart', 'client.cart');
-Route::view('/login', 'client.login');
-Route::view('/forgot-password', 'client.forgot-password');
-Route::view('/myaccount', 'client.my-account');
+Route::view('/home', 'client.home')->name('pages.home');
+Route::view('/shop', 'client.shop')->name('pages.shop');
+Route::view('/product', 'client.product-detail')->name('pages.productDetail');
+Route::view('/empty', 'client.empty')->name('pages.empty');
+Route::view('/checkout', 'client.checkout')->name('pages.checkout');
+Route::view('/order-success', 'client.order-success')->name('pages.orderSuccess');
+Route::view('/wishlist', 'client.wishlist')->name('pages.wishlist');
+Route::view('/cart', 'client.cart')->name('pages.cart');
+Route::view('/login', 'client.login')->name('pages.login');
+Route::view('/forgot-password', 'client.forgot-password')->name('pages.forgotPass');
+Route::view('/myaccount', 'client.my-account')->name('pages.myAccount');
