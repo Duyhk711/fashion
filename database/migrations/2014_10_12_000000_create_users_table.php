59fea178014f41e1c586ebdd445ab7c5a8b7fd77
doan2202 <trensky66@gmail.com>
2024-09-16 23:46:12 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin' ,'user'])->default('user');
+            $table->boolean('is_active')->default(1);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
